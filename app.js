1bb803e7f17693ec9d132e0f08f0b7050bad703f
ethornbury <ethornbury@eircom.net>
2018-11-07 20:33:48 +0000

    initial commit

--- app.js
+++ app.js
@@ -237,32 +237,25 @@ app.post('/edit/:name', function(req, res){
 
 // // end delete function
 
-const MongoClient = require('mongodb').MongoClient;
+
+//const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+//// Connection URL
+//const url = 'mongodb://localhost:27017';
+//// Database Name
+//const dbName = 'myproject';
  
-// Connection URL
-const url = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = 'myproject';
- 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
-
-
-
+//// Use connect method to connect to the server
+//MongoClient.connect(url, function(err, client) {
+//  assert.equal(null, err);
+//  console.log("Connected successfully to server");
+//  const db = client.db(dbName);
+//  client.close();
+//});
 
 
 // This function gets the application up and running on the development server.
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("Yippee its running");
-  
 })
 
